d03c1f65608c8f5541f1c4c22a6e8db583dce687
laiyongtao <laiyongtao6908@163.com>
2021-07-03 17:30:40 +0800

    use nameko's error deserialize

--- aio_nameko_proxy/excs.py
+++ aio_nameko_proxy/excs.py
@@ -1,32 +1,8 @@
 # coding=utf-8
+from nameko.exceptions import RemoteError, deserialize, deserialize_to_instance
 
 class ConfigError(Exception):
     pass
 
 class ClientError(Exception):
     pass
-
-class RemoteError(Exception):
-    """ Exception to raise at the caller if an exception occurred in the
-    remote worker.
-    """
-
-    def __init__(self, exc_type=None, value=""):
-        self.exc_type = exc_type
-        self.value = value
-        message = '{} {}'.format(exc_type, value)
-        super(RemoteError, self).__init__(message)
-
-
-def deserialize(data):
-    """ Deserialize `data` to an exception instance.
-
-    If the `exc_path` value matches an exception registered as
-    ``deserializable``, return an instance of that exception type.
-    Otherwise, return a `RemoteError` instance describing the exception
-    that occurred.
-    """
-
-    exc_type = data.get('exc_type')
-    value = data.get('value')
-    return RemoteError(exc_type=exc_type, value=value)
